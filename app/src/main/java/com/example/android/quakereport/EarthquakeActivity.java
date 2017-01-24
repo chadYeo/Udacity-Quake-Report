c2170d7b6d511fcdf6e29b84b118164646a319af
chadyeo <chad.yeo.cy@gmail.com>
2017-01-23 22:29:24 -0500

    Added QueryUtils

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -15,13 +15,8 @@ public class EarthquakeActivity extends AppCompatActivity {
         setContentView(R.layout.earthquake_main);
 
         // Create a fake list of earthquake locations.
-        ArrayList<Earthquake> earthquakes = new ArrayList<Earthquake>();
-        earthquakes.add(new Earthquake(7.2, "San Francisco", "Feb 2, 2016"));
-        earthquakes.add(new Earthquake(6.1, "Tokyo", "July 2, 2016"));
-        earthquakes.add(new Earthquake(3.9, "Mexico City", "Nov 2, 2016"));
-        earthquakes.add(new Earthquake(5.4, "Moscow", "May 2, 2016"));
-        earthquakes.add(new Earthquake(2.8, "Rio de Janeiro", "Jan 2, 2016"));
-        earthquakes.add(new Earthquake(1.6, "Paris", "Oct 2, 2016"));
+        ArrayList<Earthquake> earthquakes = new QueryUtils().extractEarthquakes();
+
 
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
