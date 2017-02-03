2976ffa6674f12a62c59c2f5c3bb5f31b5e30a2e
Chad Yeo <chad.yeo.cy@gmail.com>
2017-02-03 16:35:38 -0500

    Added ConnectivityManager

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -3,6 +3,8 @@ package com.example.android.quakereport;
 import android.app.LoaderManager;
 import android.content.Intent;
 import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +12,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,8 +21,11 @@ import java.util.List;
 public class EarthquakeActivity extends AppCompatActivity
         implements LoaderManager.LoaderCallbacks<List<Earthquake>>{
 
+    private ProgressBar mProgressBar;
     private EarthquakeAdapter mAdapter;
+    private TextView mEmptyTextView;
 
+    private ListView mEarthquakeListView;
     private static final String LOG_TAG = Earthquake.class.getName();
 
     private static final int EARTHQUAKE_LOADER_ID = 1;
@@ -30,13 +37,17 @@ public class EarthquakeActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_main);
 
-        ListView earthquakeListView = (ListView) findViewById(R.id.list);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+        mProgressBar.isIndeterminate();
+        mEarthquakeListView = (ListView) findViewById(R.id.list);
+        mEmptyTextView = (TextView) findViewById(R.id.empty_list_view);
 
         mAdapter = new EarthquakeAdapter(this, new ArrayList<Earthquake>());
 
-        earthquakeListView.setAdapter(mAdapter);
+        mEarthquakeListView.setAdapter(mAdapter);
+        mEarthquakeListView.setEmptyView(mEmptyTextView);
 
-        earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        mEarthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
@@ -50,9 +61,17 @@ public class EarthquakeActivity extends AppCompatActivity
             }
         });
 
-        LoaderManager loaderManager = getLoaderManager();
-        loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
-        Log.i(LOG_TAG, "loaderManger.initLoader is initiated");
+        ConnectivityManager cm = (ConnectivityManager)getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            LoaderManager loaderManager = getLoaderManager();
+            loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
+            Log.i(LOG_TAG, "loaderManger.initLoader is initiated");
+        } else {
+            mProgressBar.setVisibility(View.GONE);
+            mEmptyTextView.setText(R.string.no_internet_connection);
+        }
     }
 
     @Override
@@ -63,6 +82,9 @@ public class EarthquakeActivity extends AppCompatActivity
 
     @Override
     public void onLoadFinished(Loader<List<Earthquake>> loader, List<Earthquake> data) {
+        mEmptyTextView.setText(R.string.no_earthquakes);
+        mProgressBar.setVisibility(View.GONE);
+
         if (data != null && !data.isEmpty()) {
             mAdapter.addAll(data);
         }
